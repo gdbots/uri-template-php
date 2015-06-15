c30275b465c2c524242bf5b5b779cf5d4f1087ce
Greg Brown <greg@12dnetworks.com>
2015-06-15 15:56:25 -0700

    add youtube playlist to templates

--- uri-templates.php
+++ uri-templates.php
@@ -8,6 +8,9 @@
 use \Gdbots\UriTemplate\UriTemplateService;
 
 UriTemplateService::registerTemplates([
+    'youtube:playlist.canonical' => 'http://www.youtube.com/watch?v={youtube_video_id}&amp;list={youtube_playlist_id}',
+    'youtube:playlist.player_url' => 'https://www.youtube.com/embed/{youtube_video_id}?list={youtube_playlist_id}',
+    'youtube:playlist.oembed' => 'http://www.youtube.com/oembed?url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D{youtube_video_id}%26list%3D{youtube_playlist_id}&amp;format={format}',
     'youtube:video.canonical' => 'http://www.youtube.com/watch?v={youtube_video_id}',
     'youtube:video.player_url' => 'https://www.youtube.com/embed/{youtube_video_id}',
     'youtube:video.oembed' => 'http://www.youtube.com/oembed?url=http%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D{youtube_video_id}&amp;format={format}',
