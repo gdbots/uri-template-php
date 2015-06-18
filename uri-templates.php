9b6bcc799dfcb9d5a0dc7890c83b5fc7b8bf3aff
Michael H. Arieli <excelwebzone@gmail.com>
2015-06-18 09:06:14 -0700

    Updated uri templates

--- uri-templates.php
+++ uri-templates.php
@@ -8,11 +8,11 @@
 use \Gdbots\UriTemplate\UriTemplateService;
 
 UriTemplateService::registerTemplates([
-    'vevo:playlist.canonical' => 'https://www.vevo.com/watch/playlist/{vevo_playlist_id}',
+    'vevo:playlist.canonical' => 'http://www.vevo.com/watch/playlist/{vevo_playlist_id}',
     'vevo:video.canonical' => 'http://www.vevo.com/watch/{vevo_video_id}',
-    'vevo:video.player_url' => 'http://cache.vevo.com/assets/html/embed.html?video={vevo_video_id}',
-    'vine:video.canonical' => 'https://vine.co/{vine_video_id}',
-    'vine:video.player_url' => 'https://vine.co/v/{vine_video_id}/embed/simple?audio=1',
+    'vevo:video.player_url' => 'http://videoplayer.vevo.com/embed/Embedded?videoId={vevo_video_id}&playlist=false&autoplay=0&playerType=embedded',
+    'vine:video.canonical' => 'https://vine.co/v/{vine_video_id}',
+    'vine:video.player_url' => 'https://vine.co/v/{vine_video_id}/card',
     'vine:video.oembed' => 'https://vine.co/oembed.{format}?url=https%3A%2F%2Fvine.co%2Fv%2F{vine_video_id}',
     'youtube:playlist.canonical' => 'http://www.youtube.com/watch?v={youtube_video_id}&amp;list={youtube_playlist_id}',
     'youtube:playlist.player_url' => 'https://www.youtube.com/embed/{youtube_video_id}?list={youtube_playlist_id}',
